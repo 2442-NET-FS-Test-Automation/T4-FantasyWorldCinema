f06216301c06fef4325dd637b0816b8d84c2d9a2
MasterDigimon <gamerdigimonxl@gmail.com>
2026-07-28 11:19:11 -0600

    Adding drawer to see cinemas from movies

--- client/cinema/src/api/Cinema.ts
+++ client/cinema/src/api/Cinema.ts
@@ -5,3 +5,8 @@ export async function getCinemas (): Promise<CinemaItem[]> {
     const response = await api.get<CinemaItem[]>("/Cinema")
     return response.data;
 }
+
+export async function getCinemasByMovie (movieId?:number): Promise<CinemaItem[]> {
+    const response = await api.get<CinemaItem[]>(`/Cinema/${movieId}`);
+    return response.data;
+}
