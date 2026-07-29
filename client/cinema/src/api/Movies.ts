ba34fa64d6571d359d8792d9f4033b490d83b219
FraJimVit <francisco.jmz.vital@gmail.com>
2026-07-28 23:22:52 -0600

    Delete method Movie Action

--- client/cinema/src/api/Movies.ts
+++ client/cinema/src/api/Movies.ts
@@ -15,3 +15,14 @@ export async function CreateMovie(movieData: CreateMovieItem): Promise<CreateMov
     const response = await api.post(`/Movies`, movieData);
     return response.data;
 }
+
+export async function DeleteMovie(movie_Id: number): Promise<void> {
+    await api.delete(`/Movies`, {
+        params: { movie_Id }
+    });
+}
+
+export async function UpdateMovie(movieData: MovieItem): Promise<MovieItem> {
+    const response = await api.put(`/Movies`, movieData);
+    return response.data;
+}
