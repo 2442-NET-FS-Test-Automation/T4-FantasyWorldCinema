74367420a4bbf867717301b5f8ab2a8866a6c0cf
MasterDigimon <gamerdigimonxl@gmail.com>
2026-08-18 11:57:19 -0600

    Create GetMovies Integration test

--- server/Tests/Integration/MoviesIntegrationTests.cs
+++ server/Tests/Integration/MoviesIntegrationTests.cs
@@ -118,7 +118,7 @@ public class MoviesIntegrationTests : IDisposable
     public async Task GetMovies_ReturnsTheCurrentMoviesWithShowtimes()
     {
         // Arrange
-        CinemaDbContext db = _factory.Services.CreateScope().ServiceProvider.GetRequiredService<CinemaDbContext>();
+        using CinemaDbContext db = _factory.Services.CreateScope().ServiceProvider.GetRequiredService<CinemaDbContext>();
         SeedTestData(db);
     
         // Act
@@ -130,6 +130,7 @@ public class MoviesIntegrationTests : IDisposable
 
         var movies = await response.Content.ReadFromJsonAsync<List<MoviesDTO>>();
         movies.Should().HaveCount(2);
-        movies[0].Title.Should().Be("Dune");
+        movies.Should().Contain(m => m.Title == "Dune");
+        movies.Should().Contain(m => m.Title == "Titanic");
     }
 }
