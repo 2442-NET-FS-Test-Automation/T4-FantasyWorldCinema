19962c7fc734b478de824cda1db1e2cbc5665398
MasterDigimon <gamerdigimonxl@gmail.com>
2026-08-20 09:19:27 -0600

    Cypress config modified

--- client/cinema/cypress.config.ts
+++ client/cinema/cypress.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from "cypress";
+import registerCodeCoverage from "@cypress/code-coverage/task";
+import getCompareSnapshotsPlugin from "cypress-image-diff-js/plugin";
 
 export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: "http://localhost:5173",
+    supportFile: "Cypress/support/e2e.js",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task", {
+        log(message) {
+            console.log(`[spec] ${message}`);
+            return null;
+        }
+      });
+      registerCodeCoverage(on, config);
+      getCompareSnapshotsPlugin(on, config);
+      return config;
     },
   },
+  component : {
+        devServer: {
+            framework: "react",
+            bundler: "vite",
+        }
+    }
 });
--- client/cinema/cypress/support/e2e.ts
+++ client/cinema/cypress/support/e2e.ts
@@ -15,3 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+import "@cypress/code-coverage/support";
+import compareSnapshotCommand from "cypress-image-diff-js";
+
+compareSnapshotCommand();
